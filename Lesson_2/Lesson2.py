34e7c6fb6aebef8629b61223d90f0398e5d108c9
Max R <max.resnick@gmail.com>
2012-02-13 21:49:02 -0500

    Lesson 2 on comments, I didn't really follow.

---
+++ Lesson_2/Lesson2.py
@@ -0,0 +1,9 @@
+#A comment, this is so you can read your program later.  Useful!!!
+#I'm writing this lesson in Nano, because it's cake.
+
+print "I could have code like this." # and the comment after is ignored.
+
+#You can alos use a coment to "disable" out a peice of code for example
+# print "You can't touch this"
+
+print "You can touch this."
