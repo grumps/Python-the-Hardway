8a6121d5c890eb2279cf593a08c78df8341f5518
Max R <max.resnick@gmail.com>
2012-02-19 21:51:27 -0500

    Lesson 9, slightly modified

---
+++ Lesson_9/Lesson9.py
@@ -0,0 +1,16 @@
+# Lesson 9 add more "strange" stuff.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#I added an \n to make the variable print on a new line, prettier.
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-qoutes.
+We'll be able to yp as much as we like.
+Even 4 lines if we want, or 5 or 6.
+"""
