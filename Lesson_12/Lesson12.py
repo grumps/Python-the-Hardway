bbec8457ab79ddee37d0866253b11dac6c9a4987
Max R <max.resnick@gmail.com>
2012-02-20 22:54:08 -0500

    Lesson 12 Complete

---
+++ Lesson_12/Lesson12.py
@@ -0,0 +1,8 @@
+# Lesson 12 Prompting People
+
+age = raw_input("Just how young are you? ")
+height = raw_input("Just how short are you? ")
+weight = raw_input("How much do you weight, no lying!? ")
+
+
+print "So, you're %r old, %r tall and %r light." % (age, height, weight)
