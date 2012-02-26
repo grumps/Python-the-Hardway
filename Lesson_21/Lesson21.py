d0e68265596c50bdda5eeb8e32d7a4891ff182e8
Max R <max.resnick@gmail.com>
2012-02-26 16:24:11 -0500

    Lesson 21 Complete

---
+++ Lesson_21/Lesson21.py
@@ -0,0 +1,35 @@
+#Lesson 21
+
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d." % (age, height, weight, iq)
+
+#Meh I'm not going to do this puzzle...
+
+print """
+here is the puzzle that I'm to lazy to do, simply start in the very center by performing the division, the 2nd var becomes '2' and so on.
+"""
+what = add(age, subtract(height, multiply(weight, divide(iq,2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
